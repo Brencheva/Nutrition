c35479a788d754e0af17a390cc4f3449d5d13bc9
Kristina.Brencheva <Kristina.Brencheva@sidenis.com>
2019-10-28 12:54:47 +0700

    Extended cuisine's enum

--- src/app/interfaces/recipe.ts
+++ src/app/interfaces/recipe.ts
@@ -86,7 +86,20 @@ export enum CuisineType {
   ASIAN = 'Asian',
   BRITICH = 'British',
   CARIBBEAN = 'Caribbean',
-  CENTRAL_EUROPE = 'Central Europe'
+  CENTRAL_EUROPE = 'Central Europe',
+  CHINECE = 'Chinese',
+  EASTERN_EUROPE = 'Eastern Europe',
+  FRENCH = 'French',
+  INDIAN = 'Indian',
+  ITALIAN = 'Italian',
+  JAPANESE = 'Japanese',
+  KOSHER = 'Kosher',
+  MEDITERRANEAN = 'Mediterranean',
+  MEXICAN = 'Mexican',
+  MIDDLE_EASTERN = 'Middle Eastern',
+  NORDIC = 'Nordic',
+  SOUTH_AMERICAN = 'South American',
+  SOUTH_EAST_ASIAN = 'South East Asian'
 }
 
 export interface FilterValue {
